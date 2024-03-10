50cf76ecf3fa8c8e956ca5e1d57d4f873710a95c
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2024-03-10 10:05:59 +0000

    shadcn init

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-        Home is here!
+      {/* <p> Home is here! </p>
+      <Button>here</Button> */}
+      
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">Home and more</CarouselItem>
+          <CarouselItem className="basis-1/3">Home and more</CarouselItem>
+          <CarouselItem className="basis-1/3">Home and more</CarouselItem>
+          <CarouselItem className="basis-1/3">Home and more</CarouselItem>
+          <CarouselItem className="basis-1/3">Home and more</CarouselItem>
+
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
     </div>
   )
 }
